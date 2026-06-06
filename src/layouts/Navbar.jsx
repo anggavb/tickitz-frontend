59abf41da3a3c106649689d88cef704976e972ed
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-06-06 10:55:05 +0700

    feat : new logo

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -9,9 +9,6 @@ const Navbar = () => {
         {/* Logo/Brand */}
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={logo} className="h-9 w-auto" alt="Logo" />
-          <span className="self-center text-2xl font-bold whitespace-nowrap text-gray-900 dark:text-white">
-            Tickitz
-          </span>
         </a>
 
         {/* Action Buttons */}
