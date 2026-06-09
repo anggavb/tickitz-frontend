efcb80a44369b43fc6f9edf3df3a71bad6981165
nopalllfd <Naufalfadhilah689@gmail.com>
2026-06-09 11:11:32 +0700

    chore: message to implement RBAC

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -3,6 +3,9 @@ import ProfileNavbar from '../components/ProfileNavbar';
 import { Outlet } from 'react-router';
 
 function AdminLayout() {
+  // nanti disini pengecekkan role
+  // const {user} = useSelector(state => state.auth)
+  // if user.role !== 'admin' navigate to user profile
   return (
     <>
       <ProfileNavbar role="admin" />
