8ff1006998729b4b3d8255438f0e1353763b3ccf
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-06-12 01:23:35 +0700

    ref : remove un used code

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -13,81 +13,6 @@ function LandingPage() {
     "https://image.tmdb.org/t/p/w500/rweIrveL43TaxUN0akQEaAXL6x0.jpg",
   ];
 
-  const movies = [
-    {
-      id: 1,
-      title: "Black Widow",
-      genre: "Action, Adventure",
-      recommended: false,
-      poster:
-        "https://image.tmdb.org/t/p/original/qAZ0pzat24kLdO3o8ejmbLxyOac.jpg",
-    },
-    {
-      id: 2,
-      title: "The Witches",
-      genre: "Comedy, Adventure",
-      recommended: true,
-      poster:
-        "https://upload.wikimedia.org/wikipedia/en/1/14/Tenet_movie_poster.jpg",
-    },
-    {
-      id: 3,
-      title: "Tenet",
-      genre: "Action, Sci-Fi",
-      recommended: true,
-      poster: "https://image.tmdb.org/t/p/w500/k68nPLbIST6NP96JmTxmZijEvCA.jpg",
-    },
-    {
-      id: 4,
-      title: "Spiderman",
-      genre: "Action, Adventure",
-      recommended: false,
-      poster: "https://image.tmdb.org/t/p/w500/rweIrveL43TaxUN0akQEaAXL6x0.jpg",
-    },
-  ];
-
-  const upcomingMovies = [
-    {
-      id: 1,
-      title: "Black Widow",
-      date: "December 2026",
-      genre: "Action, Adventure",
-      poster:
-        "https://upload.wikimedia.org/wikipedia/en/e/e9/Black_Widow_%282021_film%29_poster.jpg",
-    },
-    {
-      id: 2,
-      title: "The Witches",
-      date: "January 2027",
-      genre: "Comedy, Adventure",
-      poster: "https://upload.wikimedia.org/wikipedia/en/1/1a/Witches2020.jpg",
-    },
-    {
-      id: 3,
-      title: "Tenet",
-      date: "June 2027",
-      genre: "Action, Sci-Fi",
-      poster:
-        "https://upload.wikimedia.org/wikipedia/en/1/14/Tenet_movie_poster.jpg",
-    },
-    {
-      id: 4,
-      title: "Spiderman",
-      date: "March 2027",
-      genre: "Action, Adventure",
-      poster:
-        "https://upload.wikimedia.org/wikipedia/commons/6/60/Spiderman.JPG",
-    },
-    {
-      id: 5,
-      title: "Spiderman 2",
-      date: "March 2027",
-      genre: "Action, Adventure",
-      poster:
-        "https://upload.wikimedia.org/wikipedia/commons/6/60/Spiderman.JPG",
-    },
-  ];
-
   return (
     <HomeLayout>
       <section className="min-h-screen overflow-hidden bg-white text-neutral-900">
@@ -95,17 +20,9 @@ function LandingPage() {
 
         <WhyChooseUs />
 
-        <MovieSection
-          label="Movie"
-          title="Exciting Movies That Should Be Watched Today"
-          movies={movies}
-        />
+        <MovieSection />
 
-        <UpcomingMovieSection
-          label="Upcoming Movies"
-          title="Exciting Movie Coming Soon"
-          movies={upcomingMovies}
-        />
+        <UpcomingMovieSection />
       </section>
 
       <Newsletter />
