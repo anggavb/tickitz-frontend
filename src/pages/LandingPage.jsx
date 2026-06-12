8e373fce37239738aaba3b7e00e290b665f8653d
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-06-12 11:09:05 +0700

    ref : landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,17 +6,10 @@ import Newsletter from "../components/Newsletter";
 import HomeLayout from "../layouts/HomeLayout";
 
 function LandingPage() {
-  const featured = [
-    "https://image.tmdb.org/t/p/original/qAZ0pzat24kLdO3o8ejmbLxyOac.jpg",
-    "https://upload.wikimedia.org/wikipedia/en/1/14/Tenet_movie_poster.jpg",
-    "https://image.tmdb.org/t/p/w500/k68nPLbIST6NP96JmTxmZijEvCA.jpg",
-    "https://image.tmdb.org/t/p/w500/rweIrveL43TaxUN0akQEaAXL6x0.jpg",
-  ];
-
   return (
     <HomeLayout>
       <section className="min-h-screen overflow-hidden bg-white text-neutral-900">
-        <HeroSection images={featured} />
+        <HeroSection />
 
         <WhyChooseUs />
 
